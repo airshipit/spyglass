0d6eca47a1c7fbaff7421a524fccaea324895150
Ian H. Pittwood <pittwoodian@gmail.com>
2019-04-25 07:56:18 -0500

    Manifest undefined data validation
    This change verifies that manifests generated by Jinja2 do not contain
    undefined data. If Spyglass attempts to generate a manifest file that
    references undefined data, all previously created manifests will be
    deleted and an error will be thrown. Users may bypass this function by
    using the "--force" CLI option which will change all undefined data
    errors to warnings instead.
    
    Adds undefined data validation to Jinja2 manifest generation.
    
    Adds "--force" option to bypass undefined data validation.
    
    Adds tests for site_processor.py and enables tox testing/coverage.
    
    Change-Id: Iff000eb173995156fbc6b44e621c59ba4dffae35

--- spyglass/site_processors/site_processor.py
+++ spyglass/site_processors/site_processor.py
@@ -1,4 +1,4 @@
-# Copyright 2018 AT&T Intellectual Property.  All other rights reserved.
+# Copyright 2019 AT&T Intellectual Property.  All other rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,9 +14,9 @@
 
 import logging
 import os
+import shutil
 
-from jinja2 import Environment
-from jinja2 import FileSystemLoader
+import jinja2
 
 from spyglass.site_processors.base import BaseProcessor
 
@@ -25,14 +25,14 @@ LOG = logging.getLogger(__name__)
 
 class SiteProcessor(BaseProcessor):
 
-    def __init__(self, intermediary_yaml, manifest_dir):
+    def __init__(self, intermediary_yaml, manifest_dir, force_write):
         super().__init__()
         self.yaml_data = intermediary_yaml
         self.manifest_dir = manifest_dir
+        self.force_write = force_write
 
     def render_template(self, template_dir):
         """The method  renders network config yaml from j2 templates.
-
 
         Network configs common to all racks (i.e oam, overlay, storage,
         calico) are generated in a single file. Rack specific
@@ -40,38 +40,58 @@ class SiteProcessor(BaseProcessor):
         """
         # Check of manifest_dir exists
         if self.manifest_dir is not None:
-            site_manifest_dir = self.manifest_dir + "/pegleg_manifests/site/"
+            site_manifest_dir = os.path.join(
+                self.manifest_dir, 'pegleg_manifests', 'site')
         else:
-            site_manifest_dir = "pegleg_manifests/site/"
+            site_manifest_dir = os.path.join('pegleg_manifests', 'site')
         LOG.info("Site manifest output dir:{}".format(site_manifest_dir))
 
-        template_software_dir = template_dir + "/"
-        template_dir_abspath = os.path.dirname(template_software_dir)
-        LOG.debug("Template Path:%s", template_dir_abspath)
+        LOG.debug("Template Path: %s", template_dir)
 
-        for dirpath, dirs, files in os.walk(template_dir_abspath):
+        if self.force_write:
+            logging_undefined = \
+                jinja2.make_logging_undefined(LOG, base=jinja2.Undefined)
+        else:
+            logging_undefined = \
+                jinja2.make_logging_undefined(LOG, base=jinja2.StrictUndefined)
+
+        template_folder_name = os.path.split(template_dir)[1]
+        created_file_list = []
+        created_dir_list = []
+
+        for dirpath, dirs, files in os.walk(template_dir):
+            loader = jinja2.FileSystemLoader(dirpath)
             for filename in files:
-                j2_env = Environment(
+                j2_env = jinja2.Environment(
                     autoescape=True,
-                    loader=FileSystemLoader(dirpath),
-                    trim_blocks=True)
+                    loader=loader,
+                    trim_blocks=True,
+                    undefined=logging_undefined)
                 j2_env.filters["get_role_wise_nodes"] = \
                     self.get_role_wise_nodes
                 templatefile = os.path.join(dirpath, filename)
-                outdirs = dirpath.split("templates")[1]
+                LOG.debug("Template file: %s", templatefile)
+                outdirs = dirpath.split(template_folder_name)[1].lstrip(os.sep)
+                LOG.debug("outdirs: %s", outdirs)
 
-                outfile_path = "{}{}{}".format(
+                outfile_path = os.path.join(
                     site_manifest_dir, self.yaml_data["region_name"], outdirs)
-                outfile_yaml = templatefile.split(".j2")[0].split("/")[-1]
-                outfile = outfile_path + "/" + outfile_yaml
+                LOG.debug("outfile path: %s", outfile_path)
+                outfile_yaml = os.path.split(templatefile)[1]
+                outfile_yaml = os.path.splitext(outfile_yaml)[0]
+                outfile = os.path.join(outfile_path, outfile_yaml)
+                LOG.debug("outfile: %s", outfile)
                 outfile_dir = os.path.dirname(outfile)
                 if not os.path.exists(outfile_dir):
                     os.makedirs(outfile_dir)
+                    created_dir_list.append(outfile_dir)
                 template_j2 = j2_env.get_template(filename)
                 try:
                     out = open(outfile, "w")
-                    template_j2.stream(data=self.yaml_data).dump(out)
+                    created_file_list.append(outfile)
                     LOG.info("Rendering {}".format(outfile_yaml))
+                    rendered = template_j2.render(data=self.yaml_data)
+                    out.write(rendered)
                     out.close()
                 except IOError as ioe:
                     LOG.error(
@@ -79,3 +99,8 @@ class SiteProcessor(BaseProcessor):
                     raise SystemExit(
                         "Error when generating {:s}:\n{:s}".format(
                             outfile, ioe.strerror))
+                except jinja2.UndefinedError as e:
+                    LOG.info('Undefined data found, rolling back changes...')
+                    out.close()
+                    shutil.rmtree(site_manifest_dir)
+                    raise e
